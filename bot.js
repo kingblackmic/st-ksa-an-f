1f2e2ed45a2ee75ff3ecbfde7d5f2581c2c6fbc5
kingblackmic <37343023+kingblackmic@users.noreply.github.com>
2018-03-29 15:32:08 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,22 +9,26 @@ const client = new Discord.Client();
 
 client.on("ready", () => {
 client.user.setPresence({
-  status: 'online', 
+  status: 'online', //الحال
   game: { 
      type: 0,
      name: 'WEEK_END 🔥',    
      details: `🎈خميس ومالي خلق ازعل🎈`,  
      url: 'http://twitch.tv/Streammingg',
      state: `انا فوق السحاب هناك شوفوني ☁`,
-    application_id: '420956479747784715', 
-        small_image: `421232881684316160`,  
-        small_text: 'Hi' ,
-        large_image: `421233046612869120`, 
-        large_text: `Hi` } 
+    application_id: '420956479747784715', //ايدي البوت اللي رافع عليه الصور
+     assets: {
+        small_image: `421232881684316160`, //ايدي الصوره الصغيره
+        small_text: 'Hi' ,//الكلام اللي يضهر على الصوره الصغيره
+        large_image: `421233046612869120`, //ايدي الصوره الكبيره
+        large_text: `Hi` } //الكلام اللي يضهر على الصوره الكبيره
+
   }
     });
 });
 
 
 
- client.login("MzQ2MTMxMzc1OTkwMjQzMzQw.DZ5dZg.Zl1QnGA8Knt0rLlyAhmRq6Ad4pA");
+ client.login("MzQ2MTMxMzc1OTkwMjQzMzQw.DZ5dZg.Zl1QnGA8Knt0rLlyAhmRq6Ad4pA"); //توكن حسابك
+
+
