7cc4926c21bc71e879f5eb257745bfd15b12ee9e
kingblackmic <37343023+kingblackmic@users.noreply.github.com>
2018-03-29 15:10:32 +0400

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,11 @@
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
+
+client.login('token');
