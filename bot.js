d21f3beef48f37c0df2a3f5b2982918e9fca4919
kingblackmic <37343023+kingblackmic@users.noreply.github.com>
2018-03-29 15:18:31 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,11 +1,30 @@
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
+const Discord = require("discord.js");
+const client = new Discord.Client();
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
+
+
+
+
+
+
+client.on("ready", () => {
+client.user.setPresence({
+  status: 'online', 
+  game: { 
+     type: 0,
+     name: 'WEEK_END 🔥',    
+     details: `🎈خميس ومالي خلق ازعل🎈`,  
+     url: 'http://twitch.tv/Streammingg',
+     state: `انا فوق السحاب هناك شوفوني ☁`,
+    application_id: '420956479747784715', 
+        small_image: `421232881684316160`,  
+        small_text: 'Hi' ,
+        large_image: `421233046612869120`, 
+        large_text: `Hi` } 
   }
+    });
 });
 
-client.login('token');
+
+
+ client.login("token");
