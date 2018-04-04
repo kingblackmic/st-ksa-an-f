cd12ebc5dcb374c11d3c9b42dcf62e9981859ec2
kingblackmic <37343023+kingblackmic@users.noreply.github.com>
2018-04-04 20:03:04 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -54,7 +54,45 @@ client.on("message", message => {
 
 
 
+client.on('message',function(message) {
+                  if(!message.channel.guild) return;
 
+  const prefix = "+";
+                    if (message.content === prefix + "discrim") {
+    let messageArray = message.content.split(" ");
+    let args = messageArray.slice(1);
+    
+    if (message.author.bot) return;
+    
+    var discri = args[0]
+    let discrim
+    if(discri){
+    discrim = discri;
+    }else{
+    discrim = message.author.discriminator;
+    }
+    if(discrim.length == 1){
+        discrim = "000"+discrim
+    }
+    if(discrim.length == 2){
+        discrim = "00"+discrim
+    }
+    if(discrim.length == 3){
+        discrim = "0"+discrim
+    }
+
+        const users = client.users.filter(user => user.discriminator === discrim).map(user => user.username);
+        return message.channel.send(`
+            **Found ${users.length} users with the discriminator #${discrim}**
+            ${users.join('\n')}
+        `);
+
+/*if(command == "emoji-img"){
+        let emojis = msg.guild.emojis
+  msg.channel.send({ files: [emoji.url] });
+}*/
+}
+});
 
 
 
